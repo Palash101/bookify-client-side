7d26df751b9ffa4cb44ad102f89b6f7385ebd686
Neha Sharma <neha@craftandcode.in>
2026-05-16 11:05:30 +0530

    cors changes

--- app/core/settings.py
+++ app/core/settings.py
@@ -53,6 +53,7 @@ class Settings(BaseSettings):
         "http://127.0.0.1:5173",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "https://bookify-web-app-fawn.vercel.app"
     ]
     
     # Email
